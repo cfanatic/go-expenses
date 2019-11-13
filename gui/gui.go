edcffd8952c50d8d51cc520f57167ffee3ced083
Arnd <git@codefanatic.de>
2019-11-13 21:55:16 +0100

    Implement label method

--- gui/gui.go
+++ gui/gui.go
@@ -1,6 +1,11 @@
 package gui
 
 import (
+	"fmt"
+
+	"github.com/cfanatic/go-expense/account"
+	"github.com/cfanatic/go-expense/database"
+	"github.com/cfanatic/go-expense/datasheet"
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/gui"
 	"github.com/therecipe/qt/widgets"
@@ -9,15 +14,18 @@ import (
 type Gui struct {
 	widgets.QWidget
 
-	qApp     *widgets.QApplication
-	hlayout  *widgets.QHBoxLayout
-	vlayout  *widgets.QVBoxLayout
-	tview    *widgets.QTableView
-	tviewm   *gui.QStandardItemModel
-	bconnect *widgets.QPushButton
-	bload    *widgets.QPushButton
-	bsave    *widgets.QPushButton
-	bprint   *widgets.QPushButton
+	qApp    *widgets.QApplication
+	hlayout *widgets.QHBoxLayout
+	vlayout *widgets.QVBoxLayout
+	tview   *widgets.QTableView
+	bload   *widgets.QPushButton
+	bsave   *widgets.QPushButton
+	bprint  *widgets.QPushButton
+	bquit   *widgets.QPushButton
+	sitem   *gui.QStandardItemModel
+
+	ds *datasheet.Datasheet
+	db *database.Database
 
 	_ func() `constructor:"init"`
 
@@ -27,7 +35,7 @@ type Gui struct {
 func (w *Gui) init() {
 	geometry := widgets.QApplication_Desktop().AvailableGeometry(0)
 	w.SetWindowTitle("go-expensegui")
-	w.SetGeometry2(0, 0, 600, 600)
+	w.SetGeometry2(0, 0, 1350, 600)
 	w.Move2((geometry.Width()-w.Width())/2, (geometry.Height()-w.Height())/2)
 
 	w.ConnectKeyPressEvent(w.keypressevent)
@@ -35,37 +43,74 @@ func (w *Gui) init() {
 	w.hlayout = widgets.NewQHBoxLayout()
 	w.vlayout = widgets.NewQVBoxLayout()
 	w.tview = widgets.NewQTableView(nil)
-	w.tviewm = gui.NewQStandardItemModel(nil)
-	w.bconnect = widgets.NewQPushButton2("Connect", nil)
+	w.sitem = gui.NewQStandardItemModel(nil)
 	w.bload = widgets.NewQPushButton2("Load", nil)
 	w.bsave = widgets.NewQPushButton2("Save", nil)
 	w.bprint = widgets.NewQPushButton2("Print", nil)
+	w.bquit = widgets.NewQPushButton2("Quit", nil)
 
-	w.tview.SetModel(w.tviewm)
-	w.hlayout.AddWidget(w.bconnect, 0, 0)
+	w.tview.SetModel(w.sitem)
 	w.hlayout.AddWidget(w.bload, 0, 0)
 	w.hlayout.AddWidget(w.bsave, 0, 0)
 	w.hlayout.AddWidget(w.bprint, 0, 0)
+	w.hlayout.AddWidget(w.bquit, 0, 0)
 	w.vlayout.AddWidget(w.tview, 0, 0)
 	w.vlayout.AddLayout(w.hlayout, 0)
 
 	w.SetLayout(w.vlayout)
 
-	w.bconnect.ConnectClicked(w.connect)
+	w.bload.ConnectClicked(w.label)
+	w.bquit.ConnectClicked(func(bool) { w.qApp.Exit(0) })
+	w.tview.HorizontalHeader().ConnectSectionResized(
+		func(idx, old, new int) { fmt.Printf("Index: %d, Size: %d\n", idx, new) },
+	)
 }
 
-func (w *Gui) connect(bool) {
-	header := []string{"Date", "Payee", "Amount", "Label"}
-	items := []*gui.QStandardItem{}
-	// item1 := gui.NewQStandardItem2("Test1")
-	// item2 := gui.NewQStandardItem2("Test2")
-	// item3 := gui.NewQStandardItem2("Test3")
-	// item4 := gui.NewQStandardItem2("Test4")
-	// items = append(items, item1, item2, item3, item4)
-	w.tviewm.SetHorizontalHeaderLabels(header)
-	w.tviewm.AppendRow(items)
-	w.tview.HorizontalHeader().SetSectionResizeMode(widgets.QHeaderView__Stretch)
-	w.tview.VerticalHeader().SetSectionResizeMode(widgets.QHeaderView__Stretch)
+func (w *Gui) label(bool) {
+	var export []datasheet.Content
+	var err error
+
+	if f := widgets.QFileDialog_GetOpenFileNames(nil, "Open datasheet", core.QDir_HomePath(), "*.xlsx", "", 0); len(f) > 0 {
+		w.ds = datasheet.New(f[0], account.TAB)
+		w.db = database.New(account.ADDRESS, account.NAME, account.COLLECT)
+	} else {
+		return
+	}
+
+	if export, err = w.ds.Content(); err != nil {
+		panic("Error during datasheet export!")
+	}
+
+	for _, trans := range export {
+		items := []*gui.QStandardItem{}
+		if item, err := w.db.Document("payee", trans.Payee); err == nil {
+			items = append(items,
+				gui.NewQStandardItem2(trans.Date),
+				gui.NewQStandardItem2(trans.Payee),
+				gui.NewQStandardItem2(fmt.Sprintf("%.2f", trans.Amount)),
+				gui.NewQStandardItem2(item.Label),
+				gui.NewQStandardItem2(trans.Description),
+			)
+		} else {
+			items = append(items,
+				gui.NewQStandardItem2(trans.Date),
+				gui.NewQStandardItem2(trans.Payee),
+				gui.NewQStandardItem2(fmt.Sprintf("%.2f", trans.Amount)),
+				gui.NewQStandardItem2(""),
+				gui.NewQStandardItem2(trans.Description),
+			)
+		}
+		w.sitem.AppendRow(items)
+	}
+
+	w.sitem.SetHorizontalHeaderLabels([]string{"Date", "Payee", "Amount", "Label", "Description"})
+	w.tview.HorizontalHeader().SetSectionResizeMode(widgets.QHeaderView__Interactive)
+	w.tview.HorizontalHeader().ResizeSection(0, 100)
+	w.tview.HorizontalHeader().ResizeSection(1, 400)
+	w.tview.HorizontalHeader().ResizeSection(2, 100)
+	w.tview.HorizontalHeader().ResizeSection(3, 200)
+	w.tview.HorizontalHeader().ResizeSection(4, 500)
+	w.tview.VerticalHeader().SetSectionResizeMode(widgets.QHeaderView__Fixed)
 }
 
 func (w *Gui) keypressevent(e *gui.QKeyEvent) {
